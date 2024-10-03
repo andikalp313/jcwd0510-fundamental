616f7a86834a3aec543835721f5662e54402f1c9
andikalp313 <andikaluhurpambudi@gmail.com>
2024-10-03 12:02:08 +0700

    exercise day 3

---
+++ exercise/day3.ts
@@ -0,0 +1,3 @@
+// 1. git add .
+// 2. git commit -m "exercise day 3"
+// 3. git push origin main
--- exercise/perintahgit.js
+++ exercise/perintahgit.js
@@ -1,3 +1,3 @@
 // git config --global user.name "andikalp313"
 // git config --global user.email "andikaluhurpambudi@gmail.com"
-//
+// git commi
